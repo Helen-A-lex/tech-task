7c40bc28f9d13466b761d6574826a881d6fef000
Helen-A-lex <117782884+Helen-A-lex@users.noreply.github.com>
2023-10-13 23:45:52 +0000

    Deploying to gh-pages from @ Helen-A-lex/tech-task@937f767676b3f86b92c46c5cae81fb910b4d3009 🚀

---
+++ assets/HomePage-8c6844db.js
@@ -0,0 +1,32 @@
+import{s as t,j as e}from"./index-09138954.js";const a="/tech-task/assets/bg-laptop-e6684906.webp";t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 500px;
+`;const o=t.h1`
+  margin: 0;
+  color: #ffffff;
+  text-shadow:
+    0 0 5px #cb7aaf,
+    0 0 10px #bf40b6,
+    0 0 20px #cb7aaf,
+    0 0 40px #cb7aaf,
+    0 0 80px #cb7aaf;
+  text-align: center;
+  font-size: 85px;
+  /* margin-top: 160px; */
+`,n=t.section`
+  /* padding: 50px; */
+
+  background-image: url(${a});
+
+  background-size: cover;
+  background-position: right center;
+  background-repeat: no-repeat;
+
+  height: calc(100vh - 50px);
+  overflow: hidden;
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+`;function i(){return e.jsx(n,{children:e.jsx(o,{children:"Welcome to"})})}export{i as default};
